20ab9fc36b23ec0bbeddc6778948de0b4f1460a9
goodbit22 <matiri0@o2.pl>
2023-03-14 20:36:51 +0100

    Terraform (#6)
    * Terraform
    
    * test
    
    * Add variable libvirt_uri
    
    * Change variable type
    
    * Add static ip address
    
    * unhardcode username and do not use passwords
    
    Signed-off-by: Marcin Fabrykowski <git@fabrykowski.pl>
    
    ---------
    
    Signed-off-by: Marcin Fabrykowski <git@fabrykowski.pl>
    Co-authored-by: goodbit22 <kisspling@gmail.com>
    Co-authored-by: Marcin Fabrykowski <git@fabrykowski.pl>

---
+++ terraform/main.tf
@@ -0,0 +1,66 @@
+terraform {
+    required_providers {
+        libvirt = {
+            source = "dmacvicar/libvirt"
+        }
+    }
+    required_version = ">= 0.12"
+}
+
+provider "libvirt" {
+    uri = "${var.libvirt_uri}"
+}
+
+resource "libvirt_volume" "master_disk" {
+    name = "${var.name_disk}"
+    source = "${var.source_images}"
+    #base_volume_name = "CentOS-Stream-GenericCloud-9-20230116.0.x86_64.qcow2"
+    format = "qcow2"
+}
+
+data "template_file" "user_data" {
+    template = file("${path.module}/cloud_init.cfg")
+    vars = {
+        shell = var.shell
+        ssh_keys = var.ssh_keys
+        groups = var.groups
+        hostname = var.hostname
+        username = var.username
+    }
+}
+
+data "template_file" "network_config" {
+  template = file("${path.module}/network_config.cfg")
+    vars = {
+        ip_address = var.ip_address
+        gateway = var.gateway
+    }
+}
+
+
+resource "libvirt_cloudinit_disk" "common_init" {
+    name = "common_init.iso"
+    user_data = data.template_file.user_data.rendered
+    network_config = data.template_file.network_config.rendered
+}
+
+
+
+resource "libvirt_domain" "default" {
+    name = var.name
+    description = "Terraform Devops"
+    network_interface {
+       network_name =  "default"
+    }
+    cpu {
+        mode = "host-passthrough"
+    }
+    qemu_agent = true
+    memory = var.vm_memory
+    vcpu = var.vm_cpu
+    disk {
+        volume_id = libvirt_volume.master_disk.id
+        scsi = "true"
+    }
+    cloudinit = libvirt_cloudinit_disk.common_init.id
+}
---
+++ terraform/variable.tf
@@ -0,0 +1,89 @@
+variable "libvirt_uri"{
+  description = "Libvirt Uri"
+  type = string
+  default  = "qemu:///system"
+}
+
+variable "name" {
+  type = string 
+  default = "goodbit22_vm"
+}
+
+variable "vm_memory" {
+    description = "amount of memory" 
+    type = number 
+    default = 4096
+    validation {
+        condition = var.vm_memory >= 2048
+        error_message = "Must be 2048 or more."
+    }
+}
+
+variable "vm_cpu" { 
+  description = "Number of cpus" 
+  type = number 
+  default = 2 
+  validation {
+    condition = var.vm_cpu >= 1
+    error_message = "Must be 1 or more."
+  }
+}
+
+variable "name_disk" {
+  type = string
+  default = "Centos9_stream_master"
+}
+
+variable "source_images" {
+  description = "url image"
+  type = string
+  default = "https://cloud.centos.org/centos/9-stream/x86_64/images/CentOS-Stream-GenericCloud-9-20230116.0.x86_64.qcow2"
+}
+
+
+
+# CLOUD INIT VARIABLES
+
+variable "shell" {
+  type = string 
+  default = "/bin/bash" 
+}
+
+variable "groups" {
+  description = "user groups"
+  type = string
+  default = "wheel"
+}
+
+variable "ssh_keys" {
+  description = "SSH keys" 
+  type = string 
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQClePKrWjz5n/0zF13gjx/10c2rv0Y4bHRiYkl7crHvqRZ9wcL429wcAACe+klUEogseavmMcAiOOqIcZJvj5wY9Ok630UU75LtCgbWCMosrWXxov7WLv46GAw9Pm5OVTXci99M6kgsoRhNOLPRbyvK6jATEPShVXj0putzOYnfWeGRLqahBYr7+gORoZ2lWaO8/CbyoJ/beaiGi0oGHMZZxbjgTo65omfMCYkf85Y5nnltGUT2HnSv10GaPoKJt41E70+G+AZAYK32BNEqJzS2kgrNee4XseGCyHOXZN0EXy3bQSFm0MyVaSa7hoqQFzvNf5bLpwHF5IYTmfGpXDKXRlq/v9FZ96mOmxZRUk5nPPPq6WoIX0GV8KzxtifMSwkS3xdQdgBz0QHpUSoZ/Css+udG80gEhZltZeE8f9wBmcSZGKjG6BfdR4dFyv74NQ2ttC8CB9JlAzUa2Z1DcngshFU2nOtuHrSwI58dzKCHY0d1qJdol3McWeWW1W7QV4k= goodbit22@goodbit22-82b"
+}
+
+variable "hostname" {
+  description = "Hostname" 
+  type = string 
+  default = "devops" 
+}
+
+
+# CLOUD INIT NETWORK VARIABLES
+
+variable "ip_address" {
+  description = "ip address"
+  type = string
+  default = "192.168.122.201/24"
+}
+
+variable "gateway" {
+  description = "gateway"
+  type = string
+  default = "192.168.122.1"
+}
+
+variable "username" {
+  description = "default user username"
+  type = string
+  default = "admin"
+}
